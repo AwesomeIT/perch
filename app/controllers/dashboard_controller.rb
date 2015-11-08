19f93f7a9b706b4976759c09259d1c1bcaa14f8c
David Stancu <dstancu@nyu.edu>
2015-11-07 22:52:14 -0500

    add is_admin? helper to user model, style some devise partials

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,5 +1,6 @@
 class DashboardController < ApplicationController
-	#before_filter :authenticate_user!
+	before_filter :authenticate_user!
+  
 	def welcome
 	end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  # Helper method to check if user is an admin
+  def is_admin?
+    self.admin
+  end
 end
